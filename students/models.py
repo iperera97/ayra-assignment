71362b8bcef3c69eeffd450c2111f63248d863fe
isuru <isurumahesh97@gmail.com>
2021-01-01 21:10:33 +0530

    init student mark model

---
+++ students/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class StudentMark(models.Model):
+
+    student_id = models.PositiveIntegerField()
+    student_name = models.CharField(max_length=64)
+    grade = models.CharField(max_length=32)
+    subject = models.CharField(max_length=64)
+    year = models.PositiveSmallIntegerField()
+    semester = models.PositiveSmallIntegerField()
+    mark = models.CharField(max_length=16)
+
+    def __str__(self):
+        return "#{self.student_id} mark"
